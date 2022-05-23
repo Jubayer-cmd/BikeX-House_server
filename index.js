e72d2f0ec10e92e6bd1063338fb8a7b15f871223
jubayer <sheikhabujubayer@gmail.com>
2022-05-23 17:00:06 +0600

    task2

--- index.js
+++ index.js
@@ -17,11 +17,59 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     await client.connect();
     console.log("DB Connected");
     const bikeCollection = client.db("bike").collection("parts");
+    const purchaseCollection = client.db("bike").collection("purchase");
+    const userCollection = client.db("bike").collection("user");
+
+    app.get("/user", verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
+    });
+
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     app.get("/parts", async (req, res) => {
       const parts = await bikeCollection.find().toArray();
@@ -33,6 +81,26 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const buying = await bikeCollection.findOne(query);
       res.send(buying);
+    });
+
+    app.post("/purchase", verifyJWT, async (req, res) => {
+      const booking = req.body;
+      const result = await purchaseCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.get("/purchase", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await purchaseCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/purchase/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await purchaseCollection.deleteOne(filter);
+      res.send(result);
     });
   } finally {
   }
