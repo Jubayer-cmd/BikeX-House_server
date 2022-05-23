cd7087cec85950b392a8a1255483c61a42a0d975
jubayer <sheikhabujubayer@gmail.com>
2022-05-23 18:08:03 +0600

    delete order

--- index.js
+++ index.js
@@ -40,6 +40,18 @@ async function run() {
     const purchaseCollection = client.db("bike").collection("purchase");
     const userCollection = client.db("bike").collection("user");
 
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    };
+
     app.get("/user", verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
@@ -61,7 +73,7 @@ async function run() {
       res.send({ result, token });
     });
 
-    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       const filter = { email: email };
       const updateDoc = {
@@ -69,6 +81,13 @@ async function run() {
       };
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
+    });
+
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
     });
 
     app.get("/parts", async (req, res) => {
@@ -96,10 +115,11 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/purchase/:email", async (req, res) => {
-      const email = req.params.email;
-      const filter = { email: email };
-      const result = await purchaseCollection.deleteOne(filter);
+    // DELETE
+    app.delete("/purchase/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await purchaseCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
